12b175fa148b86805b5446d4424cd7bd3efa52d1
LamWaiBen <lamwaiben@qq.com>
2017-03-07 09:20:55 +0800

    finsh95

--- pushBox/js/app.js
+++ pushBox/js/app.js
@@ -5,26 +5,39 @@ var game={
 	oWrap: $("#game"),
 	size:{x:10,y:8},
 	lv:0,
-	tempMap:[],
-	stepNum:0,
-	step:[],
+	tempMap :[],
+	stepNum : 0,
+	step :[],
+	init:function(){
+		this.oWrap.html("");
+		this.tempMap= [];
+		this.stepNum=0;
+		this.step=[];
+		console.log(this.lv);
+	},
 	load:function(){
-		$(".pre_lv").click(game.pre);
-		$(".next_lv").click(game.next);
+		var self = this;
+		$(".pre_lv").click(self.preLv);
+		$(".next_lv").click(self.nextLv);
+		$(".pre_step").click(self.preStep);
+		$(".restart").click(self.restart);
 		$("#select_lv").change(function(){
-			game.lv = Number($("#select_lv").val());
-			game.mapchange();
+			self.lv = Number($("#select_lv").val());
+			self.mapchange();
 		});
-		this.map(game.Data[game.lv]);
+		this.map(this.Data[this.lv]);
 		this.personControl();
 	},
 	map:function(data){
 		let x= this.size.x , y = this.size.y;
-		this.oWrap.html("");
-		this.tempMap = data;
+		this.init();
+		this.tempMap = data.map(function(data1){ //深拷贝二维数组
+			return data1.concat([]);
+		})
 		for(let i = 0 ; i < y ; i++){
-			for(let j =0 ; j < x ; j++){
+			for(let j =0 ; j < x ; j++){		
 				switch(data[i][j]){
+
 					case 0 :
 						game.createElement().className='empty';
 						break;
@@ -41,11 +54,18 @@ var game={
 						game.createElement().className='person';
 						this.step =[[j,i]];
 						break;
+					// case 5 :
+					// 	game.createElement().className='match';
+					// 	break;
+					// case 6 :
+					// 	game.createElement().className="person2";
+					// 	break;
 				}
+
 			}
 		}
 	},
-	pre:function(){
+	preLv:function(){
 		if(game.lv>0){
 			game.lv-=1;
 			game.mapchange();
@@ -53,7 +73,7 @@ var game={
 			alert("当前已是第一关");
 		}
 	},
-	next:function(){
+	nextLv:function(){
 		if(game.lv<4){
 			game.lv+=1;
 			game.mapchange();
@@ -74,119 +94,153 @@ var game={
 	personControl: function(){
 		var self = this;
 		document.onkeydown = function(ev){
-			var position = self.step[self.step.length-1],
-				x = position[0],
-				y = position[1];
 			var ev = ev || window.event,
 			    keyCode = ev.keyCode;
 			    switch(keyCode){//0：空地，1：围墙，2：箱子，3：目标点，4：人物  5:箱子匹配  6：人物位于目标点
 			    	case 37 : //left
-			    		if(self.tempMap[y][x-1] == 0 ){
-			    				self.tempMap[y][x-1] = 4 ;
-			    				self.oWrap.find("div")[y*10+x-1].className="person";
-			    			if(self.tempMap[y][x] == 6){
-			    					self.tempMap[y][x] = 3
-			    					self.oWrap.find("div")[y*10+x].className="target";
-			    				}else{
-			    					self.tempMap[y][x] = 0;
-			    					self.oWrap.find("div")[y*10+x].className="empty";
-			    				};
-			    			self.step.push([x-1,y]);  
-			    		}else if(self.tempMap[y][x-1] == 3){
-			    				self.tempMap[y][x-1] = 6 ;
-			    				self.oWrap.find("div")[y*10+x-1].className="person2";
-			    				if(self.tempMap[y][x] == 6){
-			    					self.tempMap[y][x] = 3
-			    					self.oWrap.find("div")[y*10+x].className="target";
-			    				}else{
-			    					self.tempMap[y][x] = 0;
-			    					self.oWrap.find("div")[y*10+x].className="empty";
-			    				};
-			    			self.step.push([x-1,y]);
-			    		}else if(self.tempMap[y][x-1] == 2){
-			    			if(self.tempMap[y][x-2] == 0 ){
-			    				self.tempMap[y][x-2] = 2 ;
-			    				self.tempMap[y][x-1] = 4 ;
-			    				if(self.tempMap[y][x] == 6){
-			    					self.tempMap[y][x] = 3
-			    					self.oWrap.find("div")[y*10+x].className="target";
-			    				}else{
-			    					self.tempMap[y][x] = 0;
-			    					self.oWrap.find("div")[y*10+x].className="empty";
-			    				};
-			    				self.oWrap.find("div")[y*10+x-2].className="box";
-			    				self.oWrap.find("div")[y*10+x-1].className="person";
-			    				self.step.push([x-1,y]);
-			    			}else if(self.tempMap[y][x-2] == 3){
-			    				self.tempMap[y][x-2] = 5 ;
-			    				self.tempMap[y][x-1] = 4 ;
-			    				if(self.tempMap[y][x] == 6){
-			    					self.tempMap[y][x] = 3
-			    					self.oWrap.find("div")[y*10+x].className="target";
-			    				}else{
-			    					self.tempMap[y][x] = 0;
-			    					self.oWrap.find("div")[y*10+x].className="empty";
-			    				};
-			    				self.oWrap.find("div")[y*10+x-2].className="match";
-			    				self.oWrap.find("div")[y*10+x-1].className="person";
-			    				self.step.push([x-1,y]);
-			    			}else{
-			    				return
-			    			}
-			    		}else if(self.tempMap[y][x-1] == 5 && ((self.tempMap[y][x-2] == 0)|| self.tempMap[y][x-2] ==3)){
-		    				if(self.tempMap[y][x] == 6){
-		    					self.tempMap[y][x] = 3
-		    					self.oWrap.find("div")[y*10+x].className="target";
-		    				}else{
-		    					self.tempMap[y][x] = 0;
-		    					self.oWrap.find("div")[y*10+x].className="empty";
-		    				};
-		    				if(self.tempMap[y][x-2] == 0){
-			    				self.tempMap[y][x-2] = 2;
-		    					self.oWrap.find("div")[y*10+x-2].className="box";
-		    				}else if(self.tempMap[y][x-2] ==3){
-		    					self.tempMap[y][x-2] = 5;
-		    					self.oWrap.find("div")[y*10+x-2].className="match";
-		    				}else{
-		    					return;
-		    				}
-			    			self.tempMap[y][x-1] = 6;
-		    				self.oWrap.find("div")[y*10+x-1].className="person2";
-		    				self.step.push([x-1,y]);
-			    		}else{
-			    			return
-			    		}
+			    		self.personMove({x:-1,y:0});
 			    		break;
 			    	case 38 : //up
-
+			    		self.personMove({x:0,y:-1});
 			    		break;
 			    	case 39 : // right
-
+			    		self.personMove({x:1,y:0});
 			    		break;
 			    	case 40 : //down
-
+			    		self.personMove({x:0,y:1});
 			    		break; 
 			    }
 			    
 		}
 	},
-	//1.遍历全图    2.遍历X/Y轴   3.只改变
-	personMove: function(x,y){ 
-
+	personMove: function(obj){
+		var	position = this.step[this.step.length-1],
+			x = position[0],
+			y = position[1],
+			i = obj.x,
+			j = obj.y;
+		if(y+j<0 ||y+j>7){
+			return
+		};
+		if(this.tempMap[y+j][x+i] == 0){
+			this.tempMap[y+j][x+i] = 4 ;
+			this.oWrap.find("div")[(y+j)*10+x+i].className="person";
+			this.step.push([x+i,y+j]); 
+			this.determine_current(x,y);
+		}else if(this.tempMap[y+j][x+i] == 3){
+			this.tempMap[y+j][x+i] = 6 ;
+			this.oWrap.find("div")[(y+j)*10+x+i].className="person2";
+			this.step.push([x+i,y+j]); 
+			this.determine_current(x,y);
+		}else if(this.tempMap[y+j][x+i] == 2){
+			if(this.tempMap[y+2*j][x+2*i] == 0 ){
+				this.tempMap[y+j][x+i] = 4 ;
+				this.tempMap[y+2*j][x+2*i] = 2 ;
+				this.oWrap.find("div")[(y+j)*10+x+i].className="person";
+				this.oWrap.find("div")[(y+2*j)*10+x+2*i].className="box";
+			}else if(this.tempMap[y+2*j][x+2*i] && this.tempMap[y+2*j][x+2*i] == 3){
+				this.tempMap[y+2*j][x+2*i] = 5 ;
+				this.tempMap[y+j][x+i] = 4 ;				
+				this.oWrap.find("div")[(y+2*j)*10+x+2*i].className="match";
+				this.oWrap.find("div")[(y+j)*10+x+i].className="person";
+			}else{
+				return
+			}
+			this.step.push([x+i,y+j,"box"]); 
+			this.determine_current(x,y);
+		}else if(this.tempMap[y+j][x+i] == 5 && ((this.tempMap[y+2*j][x+2*i] == 0)|| this.tempMap[y+2*j][x+2*i] ==3)){
+			if(this.tempMap[y+2*j][x+2*i] == 0){
+				this.tempMap[y+2*j][x+2*i] = 2;
+				this.oWrap.find("div")[(y+2*j)*10+x+2*i].className="box";
+			}else if(this.tempMap[y+2*j][x+2*i] ==3){
+				this.tempMap[y+2*j][x+2*i] = 5;
+				this.oWrap.find("div")[(y+2*j)*10+x+2*i].className="match";
+			}else{
+				return;
+			}
+			this.tempMap[y+j][x+i] = 6;
+			this.oWrap.find("div")[(y+j)*10+x+i].className="person2";
+			this.step.push([x+i,y+j,"box"]); 
+			this.determine_current(x,y);
+		}else{
+			return
+		};
+		this.stepNum++;
+		$(".step").html(this.stepNum);
+		this.passCheck();
 	},
-	perStep : function(){
-
+	determine_current : function(x,y){
+		var self = game;
+		if(self.tempMap[y][x] == 6){
+			self.tempMap[y][x] = 3
+			self.oWrap.find("div")[y*10+x].className="target";
+		}else{
+			self.tempMap[y][x] = 0;
+			self.oWrap.find("div")[y*10+x].className="empty";
+	};},
+	preStep : function(){
+		if(game.step.length>1){
+			var self = game,
+				currentStep = self.step[self.step.length-1],
+				preStep = self.step[self.step.length-2],
+				i = preStep[0] - currentStep[0],  //需要还原的x位移
+				j = preStep[1] - currentStep[1],
+				temp = {x:i,y:j};
+				self.personMove(temp);
+				self.step.pop();
+				self.step.pop();
+				if(currentStep[2] && currentStep[2] == "box"){
+					let boxX = currentStep[0]-i,boxY = currentStep[1]-j;
+					console.log(self.tempMap[boxY][boxX],self.tempMap[boxY+j][boxX+i]);
+					if(self.tempMap[boxY][boxX] ==2){
+						self.tempMap[boxY][boxX] = 0;
+						self.oWrap.find("div")[(boxY)*10+boxX].className="empty";
+						if(self.tempMap[boxY+j][boxX+i] == 3){
+							self.tempMap[boxY+j][boxX+i] = 5;
+							self.oWrap.find("div")[(boxY+j)*10+boxX+i].className="match";
+						}else{
+							self.tempMap[boxY+j][boxX+i] = 2;
+							self.oWrap.find("div")[(boxY+j)*10+boxX+i].className="box";
+						}
+					}else if( self.tempMap[boxY][boxX] == 5){
+						self.tempMap[boxY][boxX] = 3
+						self.oWrap.find("div")[(boxY)*10+boxX].className="target";
+						if(self.tempMap[boxY+j][boxX+i] == 3){
+							self.tempMap[boxY+j][boxX+i] = 5;
+							self.oWrap.find("div")[(boxY+j)*10+boxX+i].className="match";
+						}else{
+							self.tempMap[boxY+j][boxX+i] = 2
+							self.oWrap.find("div")[(boxY+j)*10+boxX+i].className="box";
+						}
+					}else{
+						console.log("error")
+					}
+				}
+		}else{
+			alert("已经无法再退步了！")
+		}
 	},
-	passLevel : function(){
-
+	restart : function(){
+		game.map(game.Data[game.lv]);
+	},
+	passCheck : function(){
+		var temp =this.tempMap.every(function(item){
+			return item.every(function(item2){
+				return item2!=2;
+			})
+		});
+		if(temp){
+			alert("恭喜通关");
+			this.nextLv();
+		}
 	},
 	//10x8格子  0：空地，1：围墙，2：箱子，3：目的地，4：人物
 	Data:[
 		[
-		  [0,3,0,3,3,0,3,2,0,4],
+		  [0,0,0,0,0,0,0,0,0,0],
 	      [0,0,0,1,1,1,0,0,0,0],
 	      [0,0,1,1,3,3,1,0,0,0],
-	      [0,0,1,3,2,2,0,1,0,0],
+	      [0,0,1,4,2,2,0,1,0,0],
 	      [0,0,1,0,0,0,0,1,0,0],
 	      [0,0,1,1,1,1,1,1,0,0],
 		  [0,0,0,0,0,0,0,0,0,0],
